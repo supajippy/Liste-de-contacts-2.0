2b976f7a4dc24812f2e99c82463fe2f9f496402b
supajippy <supajippy@hotmail.com>
2018-04-19 21:45:36 -0400

    Kibbles and BIts

--- src/Occupation.java
+++ src/Occupation.java
@@ -19,11 +19,27 @@ public class Occupation implements Serializable {
         System.out.println("A-t-il une occupation? 1-oui 2-non");
         if(sc.next().equals("1")){
             System.out.println("Quel est son poste?");
+            verifierString(sc.next());
             this.setPoste(sc.next());
             System.out.println("Pour quelle entreprise?");
+            verifierString(sc.next());
             ent.setNom(sc.next());
             ent.ad.assigneradresse();
         }
         else;
+    }
+    public static void verifierString(String sc){
+        int diviseur=1;
+        for(int i=0;i<sc.length();i++) {
+            if (sc.charAt(i) < 65 || sc.charAt(i) > 122) {
+                diviseur = 0;
+            }
+        }
+        try{
+            int exception = 1/diviseur;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Vous avez entré un mauvais caractère, veuillez recommencer");
+        }
     }
 }
